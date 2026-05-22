fa648caab949ded85e2edeef3a1e5ea36bc4dfec
Arpit Goswami <arpitgoswami722@gmail.com>
2026-05-22 21:58:06 +0530

    list

---
+++ 13_list.py
@@ -0,0 +1,23 @@
+
+
+a = ["rohan", 34.5 , 43 , True , False , "orange"] #can store any type of data(heterogenous) 
+# # unlike strings lists are mutable , means it can be change
+# a[0] = "apple"
+# print(a)
+# # duplicates can be added 
+# #lists are ordered which means elements can be access by their index value
+
+# #list indexing :
+# print(a[3])
+# print(a[-4])
+# #list slicing :
+# print(a[0:3])
+
+# #list traversing :
+# #1st way using index 
+# for i in range(len(a)):
+#     print(a[i])
+
+# #2nd way directly on values 
+# for i in a :
+#     print(i) #here u can't print index , that's why prefer 1st method
